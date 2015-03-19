a20c8ae6a7460b3cb3fda23822492ece10e915ab
stevedavies <steve.davies.iphone@gmail.com>
2015-03-18 22:04:15 -0500

    new class in progress...

---
+++ Classes/SetPartiallyPlayedToEnd.h
@@ -0,0 +1,13 @@
+//
+//  SetPartiallyPlayedToEnd.h
+//  AddMusic
+//
+//  Created by Steve Davies on 3/14/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SetPartiallyPlayedToEnd : NSObject
+
+@end
